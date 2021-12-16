661c80e1df6f3b8d8e3c4b455eb5bcfe0146b711
D-Biggs <dale_biggs@yahoo.com>
2021-12-15 23:32:16 -0500

    -Create Page flow complete -Edit page to be edited -minor fixes

--- Sales and Inventory System/js/Invoice/invoice.js
+++ Sales and Inventory System/js/Invoice/invoice.js
@@ -11,6 +11,9 @@ let invenDescSearch=document.getElementById("equipmentDescription");
 let equipmentResults=document.getElementById("resultsTable");
 let InvenDetailsSection=document.getElementById("invenDetails");
 let invenSearchResults= document.getElementById("invenSearchResults");
+let invenTextArea= document.getElementById("txtSelectedEquipment");
+let btnConfirmItemSelections=document.getElementById("confirmItemSelections");
+let invenLegend=document.getElementById("invenLegendLink");
 //------------------------------------
 let existingCustomerSearch=document.getElementById("searchBox");
 let btnSearch= document.getElementById("btnSearch");
@@ -23,7 +26,7 @@ let btnConfirmExistingCustomer= document.getElementById("btnConfirmExistingCusto
 //Events-----------------------------------
 // search field on New Invoice page
 btnSearch.addEventListener('click', function(){
-  let searchResults='';
+  
   $.getJSON('/json/customer.json',function(data){
       console.log(data);
       for( let i=0; i<data.length; i++){
@@ -31,7 +34,7 @@ btnSearch.addEventListener('click', function(){
         if(data[i].firstName.includes(existingCustomerSearch.value)){
           
           existingCustomerResultsTable.innerHTML+=`<tr>
-          <td><input type="radio" name="radSearchResults" id= "${data[i].id}" value="${data[i].firstName}""> ${data[i].firstName +" "+ data[i].lastName}</td>
+          <td><input type="radio" name="radSearchResults" id= "${data[i].id}" value="${data[i].firstName} ${data[i].lastName}"> ${data[i].firstName +" "+ data[i].lastName}</td>
           <td>${data[i].email}</td>
           <td>${data[i].address}, ${data[i].city}, ${data[i].province}</td>
           <td>${data[i].phone}</td>
@@ -44,6 +47,7 @@ btnSearch.addEventListener('click', function(){
         radOptions[i].addEventListener('click',function(){
           customerLegend.textContent="Customer  >"+" "+radOptions[i].value;
           billToExistingFieldset.style.display="none";
+          document.getElementById("invenLegend").style.display='block';
           InvenDetailsSection.style.display='block';
           //OrderDetailsSection.style.display='none';
         })
@@ -54,10 +58,19 @@ btnSearch.addEventListener('click', function(){
   
 
 })
+btnConfirmItemSelections.addEventListener('click', function(){
+  invenSearchResults.innerHTML='';
+  invenDescSearch.innerHTML='';
+  InvenDetailsSection.style.display='none';
+  document.getElementById('orderDetailsLegend').style.display='block';
+  OrderDetailsSection.style.display='block';
+  invenLegend.textContent= 'Inventory Selection > Complete';
+
+})
 //Search Field under the Inventory Section
 
 searchBtn.addEventListener('click',function(){
-  let results='';
+  invenSearchResults.innerHTML='';
   $.getJSON('/json/Store Inventory List/inventory.json',function(data){
     console.log(data);
     if(invenDescSearch.value !=''){
@@ -66,15 +79,16 @@ searchBtn.addEventListener('click',function(){
          invenSearchResults.innerHTML+=`<tr>
           <td>${data[i].description} - ${data[i].brand}</td>
           <td>
-            <select style=" height: auto;" id="equipmentColor" name="equipmentColor" required>
+            <select style=" height: auto;" id="select_id#${data[i].id}" name="equipmentColor" required>
             <option selected disabled>Item Color</option>
-            <option>Silver</option>
-            <option>Grey</option>
-            <option>Blue</option>
-            <option>Dull White</option>
+            <option value="${data[6].color}">Silver</option>
+            <option value="${data[0].color}">Grey</option>
+            <option value="${data[1].color}">Blue</option>
+            <option value="${data[5].color}" >Dull White</option>
             </select>
           </td>
-          <td><a href="#" name="addLink" id="${data[i].id}">Add</a></td>
+          <td><input type="radio" name="radWarranty" id= "warranty_id#${data[i].id}" value="${this.checked? true :false}"></td>
+          <td><a href="#" name="addLink" id="addLink_id#${data[i].id}">Add</a></td>
           </tr>`;
   
         }
@@ -84,18 +98,47 @@ searchBtn.addEventListener('click',function(){
         //}
       }
       let addLinks= document.getElementsByName("addLink");
-      let colourSelect=document.createElement('SELECT');
+      
+      
       for(let i=0; i<addLinks.length;i++){
+
         addLinks[i].addEventListener('click', function(){
+          btnConfirmItemSelections.style.display='block';
           for(let j=0; j<data.length;j++){
-            if(data[j].id == addLinks[i].id){
-              document.getElementById("txtSelectedEquipment").innerHTML+=`${data[j].description} - ${data[j].brand} `;
-                
+            if(`addLink_id#${data[j].id}` == addLinks[i].id && document.getElementById(`warranty_id#${data[j].id}`).checked ){
+              let color= document.getElementById(`select_id#${data[j].id}`).value;
+    
+              invenTextArea.innerHTML+= `<div id="item_id#${data[j].id}"> <select style="border: 1px solid #1f3a4d;height: auto;"  name="selectedItemsDDL">
+              <option selected >${data[j].brand} ${data[j].description}</option>
+              <option  disabled >Color:    ${color}</option>
+              <option disabled  >Warranty:   Yes</option>
+              
+              </select> <a href="#" style="color: red;"name="removeLink" id="removeLink_id#${data[j].id}">Remove</a></div>`
+              
+            }else if (`addLink_id#${data[j].id}` == addLinks[i].id && !document.getElementById(`warranty_id#${data[j].id}`).checked ){
+              let color= document.getElementById(`select_id#${data[j].id}`).value;
+    
+              invenTextArea.innerHTML+= `<div id="item_id#${data[j].id}"> <select style="border: 1px solid #1f3a4d;height: auto;"  name="selectedItemsDDL">
+              <option selected >${data[j].brand} ${data[j].description}</option>
+              <option  disabled >Color:    ${color}</option>
+              <option disabled  >Warranty:   No</option>
+              
+              </select>   <a href="#" style="color: red;"name="removeLink" id="removeLink_id#${data[j].id}">Remove</a></div>`          
+            }
+          }
+        })
+   
+      }
+      let removeLinks=document.getElementsByName("removeLink");
+      for(let i=0; i<removeLinks.length;i++){
+        removeLinks[i].addEventListener('click', function(){
+          for(let j=0; j<data.length;j++){
+            if(`removeLink_id#${data[j].id}`==removeLinks[i].id){
+              document.getElementById(`item_id#${data[j].id}`).remove();
             }
           }
         })
       }
-      //invenSearchResults.innerHTML=results;//alert(results);
 
     }
   })
@@ -110,6 +153,7 @@ radNew.onclick=function(){
   customerLegend.textContent="Customer  ^";
   
 }
+
 radExisting.onclick=function(){
     customerLegend.textContent="Customer  ^";
   billToExistingFieldset.style.display="block";
@@ -120,6 +164,43 @@ customerLegend.onmouseover=function(){
     
     customerLegend.style.color="skyblue";
     customerLegend.style.textDecoration="none";
+}
+document.getElementById('invenLegendLink').onmouseover=function(){
+  document.getElementById('invenLegendLink').style.color='skyblue';
+  document.getElementById('invenLegendLink').style.textDecoration='none';
+}
+document.getElementById('invenLegendLink').onmouseleave=function(){
+  document.getElementById('invenLegendLink').style.color='#ffffff';
+}
+document.getElementById('invenLegendLink').onclick=function(){ //Controls inventory section behaviour
+  if(document.getElementById('invenLegendLink').textContent.includes('Complete')){
+    if(document.getElementById('invenLegendLink').textContent.includes('^')){
+    
+      document.getElementById('invenLegendLink').textContent="Inventory Selection > Complete";
+      InvenDetailsSection.style.display="none";
+      
+      
+    }else if(document.getElementById('invenLegendLink').textContent.includes('>')){
+      let textContent=document.getElementById('invenLegendLink').textContent;
+      InvenDetailsSection.style.display="block";
+      
+      document.getElementById('invenLegendLink').textContent="Inventory Selection ^ Complete";
+    }
+  }else{
+    if(document.getElementById('invenLegendLink').textContent.includes('^')){
+    
+      document.getElementById('invenLegendLink').textContent="Inventory Selection > ";
+      InvenDetailsSection.style.display="none";
+      
+      
+    }else if(document.getElementById('invenLegendLink').textContent.includes('>')){
+      let textContent=document.getElementById('invenLegendLink').textContent;
+      InvenDetailsSection.style.display="block";
+      
+      document.getElementById('invenLegendLink').textContent="Inventory Selection ^";
+    }
+  }
+  
 }
 customerLegend.onmouseleave=function(){
     
