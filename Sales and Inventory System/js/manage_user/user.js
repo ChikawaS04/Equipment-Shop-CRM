e3556d0c588a724386ef53a32a5f4dc25ddb6b40
madhavjoshi1999 <92283355+madhavjoshi1999@users.noreply.github.com>
2021-12-15 23:14:43 -0500

    user manager
    Final touch ready

--- Sales and Inventory System/js/manage_user/user.js
+++ Sales and Inventory System/js/manage_user/user.js
@@ -2,9 +2,17 @@
 
 if(window.location.href == "http://127.0.0.1:5502/pages/user_Manage/edit.html")
 {
-    document.getElementById("email").value = "admin1@gmail.com";
+    document.getElementById("firstName").value = "Dannella@gmail.com";
 
-    document.getElementById("admin").checked = true;
+    document.getElementById("middleName").value = "";
+
+    document.getElementById("lastName").value = "ortans";
+
+    document.getElementById("username").value = "dortans25";
+
+    document.getElementById("email").value = "Dannella@gmail.com";
+
+    document.getElementById("salesRepresentative").checked = true;
 
 }
 
