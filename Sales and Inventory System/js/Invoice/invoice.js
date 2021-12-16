b12574c31adfda4b2c71fb538dfd927de4dc1c91
D-Biggs <dale_biggs@yahoo.com>
2021-12-16 00:35:41 -0500

    -Updated--fixed conflicts

--- Sales and Inventory System/js/Invoice/invoice.js
+++ Sales and Inventory System/js/Invoice/invoice.js
@@ -1,11 +1,8 @@
-
-//-----------------------------
 let customerLegend=document.getElementById("customerLegend");
 let billToExistingFieldset=document.getElementById("billToExisting");
 let billToNewFieldset = document.getElementById("billToNew");
 let radExisting=document.getElementById("existing");
 let radNew=document.getElementById("new");
-//--------Inventory Section------------
 let searchBtn=document.getElementById("btnEquipmentOrder");
 let invenDescSearch=document.getElementById("equipmentDescription");
 let equipmentResults=document.getElementById("resultsTable");
@@ -16,12 +13,9 @@ let btnConfirmItemSelections=document.getElementById("confirmItemSelections");
 let invenLegend=document.getElementById("invenLegendLink");
 //------------------------------------
 let existingCustomerSearch=document.getElementById("searchBox");
-let btnSearch= document.getElementById("btnSearch");
 let existingCustomerResultsTable=document.getElementById("customerResultsTable");
-
-
+let equipmentResults=document.getElementById("resultsTable");
 let OrderDetailsSection=document.getElementById("orderDetails");
-let btnConfirmExistingCustomer= document.getElementById("btnConfirmExistingCustomer");
 
 //Events-----------------------------------
 // search field on New Invoice page
@@ -216,14 +210,44 @@ customerLegend.onclick=function(){
   }else if (customerLegend.textContent=="Customer  ^" && radNew.checked){
     billToNewFieldset.style.display="none";
     customerLegend.textContent="Customer  >";     
-  }else if(customerLegend.textContent.includes("Customer  >") && radExisting.checked){
+  }else if(customerLegend.textContent=="Customer  >" && radExisting.checked){
     customerLegend.textContent="Customer  ^";
     billToExistingFieldset.style.display="block";
-  }else if(customerLegend.textContent.includes("Customer  >") && radNew.checked){
+  }else if(customerLegend.textContent=="Customer  >" && radNew.checked){
     billToNewFieldset.style.display="block";
     customerLegend.textContent="Customer  ^";
   }               
 }
 
+searchBtn.onclick=function(){
+    equipmentResults.style.display="block";
+}
 //------------------------------------------------
 //Functions--------------------------------------
+function search(){
+    let searchResults=[];
+    $.getJSON("/json/customer.json",function(data){
+        console.log(data);
+        for( let i=0; i<data.length; i++){
+            if( existingCustomerSearch.innerHTML!="" && (data[i].firstName.includes(existingCustomerSearch.innerHTML) || data[i].phone.includes(existingCustomerSearch.innerHTML))){
+                searchResults.push(`<tr>
+                    <td>${data[i].firstName +" "+ data[i].lastName}</td>
+                    <td>${data[i].email}</td>
+                    <td>${data[i].address}, ${data[i].city},\n ${data[i].province}</td>
+                    <td>${data[i].phone}</td>
+                </tr>`)
+            }
+           
+            /*if( existingCustomerSearch.innerHTML!="" && (data[i].firstName +" "+ data[i].lastName || data[i].phone).includes(existingCustomerSearch.innerHTML) ){
+               
+            }else{
+
+            }*/
+        }
+    })
+    existingCustomerSearch.onkeyup=function(){
+       alert(searchResults[0])
+    }
+
+
+}
